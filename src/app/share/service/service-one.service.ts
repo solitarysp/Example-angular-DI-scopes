689c91d0abde0c0df745ab37bd2f7cba88085d42
solit <1>
2020-06-30 21:42:45 +0700

    init

--- src/app/share/service/service-one.service.ts
+++ src/app/share/service/service-one.service.ts
@@ -2,8 +2,16 @@ import {Injectable} from '@angular/core';
 
 @Injectable()
 export class ServiceOneService {
+  private count: number;
 
   constructor() {
-    console.log('tao them service');
+    this.count = 0;
+    console.log('tao them service:', new Date());
+  }
+
+  addCount() {
+    this.count++;
+    console.log('Count tăng lên ' + this.count);
+
   }
 }
