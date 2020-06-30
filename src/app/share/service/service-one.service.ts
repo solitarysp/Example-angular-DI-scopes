bf94404da1130d833a04b18f0882c0a47bd230f1
solit <1>
2020-06-30 21:54:29 +0700

    init

--- src/app/share/service/service-one.service.ts
+++ src/app/share/service/service-one.service.ts
@@ -3,25 +3,24 @@ import {BehaviorSubject, Observable, Subject} from 'rxjs';
 
 @Injectable()
 export class ServiceOneService {
+  private logSub: Subject<number> = new BehaviorSubject<number>(0);
   private count: number;
-  private logSub: Subject<string> = new BehaviorSubject<string>('');
 
   constructor() {
-    this.count = 0;
     console.log('tao them service:', new Date());
   }
 
   addCount() {
-    this.count++;
+    this.nextLogSub(this.count++);
     console.log('Count tăng lên ' + this.count);
 
   }
 
-  getLogSub(): Observable<string> {
+  getLogSub(): Observable<number> {
     return this.logSub.asObservable();
   }
 
-  nextLogSub(value: string) {
+  nextLogSub(value: number) {
     this.logSub.next(value);
   }
 
